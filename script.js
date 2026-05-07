b28b702d74dd5520e944d91e9b8d5585f9a9d029
SpideRrpPorKk <1170918@alumno.um.edu.mx>
2026-05-07 05:31:40 -0300

    script.js

--- script.js
+++ script.js
@@ -1,159 +1,91 @@
-const canvas = document.getElementById("canvas");
-const ctx = canvas.getContext("2d");
-
-function resize() {
-  canvas.width = window.innerWidth;
-  canvas.height = window.innerHeight * 0.7; 
-}
-window.addEventListener("resize", resize);
-resize();
-
-
-let x, y, angle;
-let speed, targetSpeed, turnSpeed;
-let distance;
-let lastTime;
-let changeTimer;
-let path = [];
-
-let paused = false;
-
-function restart() {
-
-  x = Math.random() * canvas.width;
-  y = Math.random() * canvas.height;
-
-  angle = Math.random() * Math.PI * 1;
-
-  speed = 0;
-  targetSpeed = 0;
-  turnSpeed = 0;
-
-  distance = 0;
-  lastTime = performance.now();
-  changeTimer = 0;
-
-  path = [];
+body {
+  margin: 0;
+  background: rgb(24, 24, 24);
+  font-family: monospace;
+  color: #00ff99;
 }
 
-restart();
-
-
-function togglePause() {
-  paused = !paused;
-  document.getElementById("pauseBtn").textContent =
-    paused ? "PLAY" : "PAUSE";
+canvas {
+  display: block;
+  width: 100vw;
+  height: 70vh;
+  background: #0095ffa6;
+  border-bottom: 2px solid #00ff99;
 }
 
-function drawFish(x, y, angle) {
-  ctx.save();
-  ctx.translate(x, y);
-  ctx.rotate(angle);
-
-  ctx.fillStyle = "#ff9f3e";
-
-  ctx.beginPath();
-  ctx.ellipse(0, -3, 45, 10, 0, 0, Math.PI * 2 );
-  ctx.fill();
-
-  const wiggle = Math.sin(Date.now() * 0.037) * 6;
-
-  ctx.fillStyle = "#808e5d";
-  ctx.beginPath();
-  ctx.moveTo(-25, 0);
-  ctx.lineTo(-65, -32 + wiggle);
-  ctx.lineTo(-40, 12 - wiggle);
-  ctx.closePath();
-  ctx.fill();
-
-  ctx.fillStyle = "#76874c";
-  ctx.beginPath();
-  ctx.moveTo(-5, -8 +wiggle);
-  ctx.lineTo(5, -18 - wiggle);
-  ctx.lineTo(15, -6);
-  ctx.closePath();
-  ctx.fill();
-
-  ctx.fillStyle = "black";
-  ctx.beginPath();
-  ctx.arc(21, 0, 3, 0, Math.PI * 2);
-  ctx.fill();
-
-  ctx.restore();
+.dash {
+  display: flex;
+  justify-content: center;
+  gap: 20px;
+  padding: 10px;
+  flex-wrap: wrap;
 }
 
-function update(time) {
-  requestAnimationFrame(update);
-
-  if (paused) return;
-
-  const dt = (time - lastTime) / 1300;
-  lastTime = time;
-
-  if (!dt || dt > 0.1) return;
-
-
-  changeTimer -= dt;
-
-  if (changeTimer <= 0) {
-    targetSpeed = Math.random() * 710.360;
-    turnSpeed = (Math.random() -0.93345) * 0.96545;
-    changeTimer = 2.20348 + Math.random() * 35.56015;
-  }
-
-
-  let accel = Math.random() < 0.15 ? -5.90 : .90;
-  speed += (targetSpeed - speed) * accel;
-
-
-  angle += turnSpeed * dt;
-  angle += Math.sin(time * .031) * .0422;
-
-  const dx = Math.cos(angle) * speed * dt;
-  const dy = Math.sin(angle) * speed * dt;
-
-  x += dx;
-  y += dy;
-
-
-  const m = 14;
-  if (x < m || x > canvas.width - m) angle = Math.PI - angle;
-  if (y < m || y > canvas.height - m) angle = -angle;
-
-  distance += Math.sqrt(dx * dx + dy * dy);
-
- 
-  path.push({ x, y });
-
-
-  ctx.clearRect(0, 0, canvas.width, canvas.height);
-
-
-  ctx.beginPath();
-  for (let i = 0; i < path.length; i++) {
-    if (i === 0) ctx.moveTo(path[i].x, path[i].y);
-    else ctx.lineTo(path[i].x, path[i].y);
-  }
-  ctx.strokeStyle = "#ebb3ff";
-  ctx.stroke();
-
-  drawFish(x, y, angle);
-
-
-  let speedMS = speed / 100;
-
-  document.getElementById("speed").textContent =
-    speedMS.toFixed(2) + " m/s";
-
-  document.getElementById("distance").textContent =
-    (distance / 100).toFixed(2) + " m";
-
-  document.getElementById("coords").textContent =
-    `X: ${Math.round(x)}, Y: ${Math.round(y)}, θ: ${angle.toFixed(2)}`;
-
-  const maxSpeed = 15;
-  document.getElementById("speedFill").style.width =
-    Math.min(speedMS / maxSpeed, 1) * 100 + "%";
+.screen {
+  background: black;
+  border: 2px solid #00ff99;
+  padding: 10px;
+  width: 180px;
+  text-align: center;
+  box-shadow: 0 0 15px #00ff99;
 }
 
-requestAnimationFrame(update);
+.label {
+  font-size: 12px;
+  opacity: 0.7;
+}
+
+.value {
+  font-size: 28px;
+  margin-top: 5px;
+}
+
+.speed-bar {
+  width: 100%;
+  height: 15px;
+  background: #bed8cb;
+  border: 1px solid #00ff99;
+  margin: 8px 0;
+  box-shadow: 0 0 6px #00ff99 inset;
+}
+
+#speedFill {
+  height: 100%;
+  width: 0%;
+  background: #ff0040;
+  transition: width 0.1s linear;
+}
+
+button {
+  background: black;
+  color: #00ff99;
+  border: 2px solid #00ff99;
+  padding: 8px;
+  cursor: pointer;
+  font-size: 12px;
+  margin-top: 6px;
+}
+
+button:hover {
+  background: #00ff99;
+  color: black;
+}
+
+.coords {
+  border: 2px solid #ffbb4d;
+  box-shadow: 0 0 20px #ffdb4d;
+  color: #fff64d;
+}
+
+#speedSlider {
+  width: 100%;
+  margin-top: 8px;
+  accent-color: #ff00e1;
+  cursor: pointer;
+}
+
+
+#controlBox.active {
+  border-color: #ff0040;
+  box-shadow: 0 0 20px #ff0040;
+}
